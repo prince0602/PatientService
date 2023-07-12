c9432e7fb7b27296e92bdf7fe8d836d17f6b69a2
Manishnupt <manish.nupt@gmail.com>
2023-07-12 20:19:00 +0530

    swagger implementation

--- src/main/java/com/example/patientservice/cotroller/PatientController.java
+++ src/main/java/com/example/patientservice/cotroller/PatientController.java
@@ -1,11 +1,18 @@
 package com.example.patientservice.cotroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.patientservice.bl.PatientBl;
 import com.example.patientservice.uiRequest.PatientUiRequest;
 import com.example.patientservice.uiResponse.PatientUIResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
 
 
 @RestController
@@ -18,12 +25,16 @@ public class PatientController {
     @Autowired
     PatientBl bl;
 
-
+    @Operation(
+    		summary = "This endpoint is used to create patient")
     @PostMapping("/createPatient")
     public Long createPatient(@RequestBody PatientUiRequest patientUiRequest){
             return bl.createPatient(patientUiRequest);
     } 
     
+    
+    @Operation(
+    		summary = "This endpoint is used to view patient details")
     @GetMapping("/getPatientDetails/{patientId}")
 
     public PatientUIResponse getPatientDetails(@PathVariable int patientId){
